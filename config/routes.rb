bf068f947a9b727aa2958b1ca41ee34693bd1017
AspenJames <aspenjames@tqca.org>
2018-06-26 15:31:51 -0700

    fix oauth callback error

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items
-  
+
   resources :items, only: [:show, :index] do
     resources :transactions, only: [:new, :index]
   end
-  
+
   resources :users
-  
-  resources :users, only: [:show, :index] do 
+
+  resources :users, only: [:show, :index] do
     resources :items, only: [:show, :new, :index, :edit]
   end
-  
+
   root 'static#welcome'
   get '/test', to: 'static#test'
   get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
   get '/signout', to: "sessions#destroy"
   get '/signup', to: 'users#new'
   get '/auth/facebook/callback', to: 'sessions#create'
